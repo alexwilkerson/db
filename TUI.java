77dc112fe6c25651312d817bd611806c29104ced
Alexander Wilkerson <alex@uncorrected.com>
2017-11-08 09:31:54 -0600

    created object for con output

--- TUI.java
+++ TUI.java
@@ -1,5 +1,8 @@
 import java.sql.*;
 import java.util.Scanner;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 class TUI {
     public static String choice;
@@ -25,7 +28,22 @@ class TUI {
                             implemented = true;
                             query = "select per_name\n" +
                                     "from person natural join works natural join job\n" +
-                                    "where comp_id = '8' and end_date is null\n";
+                                    "where comp_id = '8' and end_date is null";
+                            break;
+                        case 2:
+                            implemented = true;
+                            query = "select per_name, pay_rate\n" +
+                                    "from person natural join works natural join job\n" +
+                                    "where comp_id = '8' and pay_type = 'salary'\n" +
+                                    "order by pay_rate desc";
+                            break;
+                        case 3:
+                            implemented = true;
+                            query = "select comp_id, sum(case when pay_type = 'salary'\n" +
+                                    "\tthen pay_rate else pay_rate*1920 end) as total_labor_cost\n\n" +
+                                    "from job natural join works\n" +
+                                    "group by comp_id\n" +
+                                    "order by total_labor_cost desc";
                             break;
                         default:
                             System.out.println();
@@ -83,7 +101,7 @@ class TUI {
         implemented = false;
         System.out.println();
         System.out.println("Query: ");
-        System.out.print(query);
+        System.out.println(query);
         System.out.println();
         try{
             // step 1 load the driver class
@@ -98,8 +116,10 @@ class TUI {
 
             // step 4 execute query
             ResultSet rs = stmt.executeQuery(q);
-            while(rs.next())
-                System.out.println(rs.getString(1));
+            List<HashMap<String,Object>> test = convertResultSetToList(rs);
+            System.out.println(test);
+            //while(rs.next())
+            //    System.out.println(rs.getString(1));
 
             // step 5 close the connection object
             con.close();
@@ -110,6 +130,22 @@ class TUI {
         }
         System.out.println();
         return true;
+    }
+    
+    public static List<HashMap<String,Object>> convertResultSetToList(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+
+        while (rs.next()) {
+            HashMap<String,Object> row = new HashMap<String, Object>(columns);
+            for(int i=1; i<=columns; ++i) {
+                row.put(md.getColumnName(i),rs.getObject(i));
+            }
+            list.add(row);
+        }
+
+        return list;
     }
 
 }
