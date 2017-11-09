b9956dad606d3eba6739c286826ec69fcf503984
Alexander Wilkerson <alex@uncorrected.com>
2017-11-08 23:03:34 -0600

    finished adding all of the queries that currently work.

--- TUI.java
+++ TUI.java
@@ -70,6 +70,19 @@ class TUI {
                                     "from has_skill natural join knowledge_skill\n" +
                                     "where per_id = 1)";
                             break;
+                        case 7:
+                            implemented = true;
+                            query = "select ks_code, ks_title, ks_level, ks_description\n" +
+                                    "from required_skill natural join knowledge_skill\n" +
+                                    "where job_code = 1";
+                            if (!runQuery(query))
+                                System.out.println("An error occurred.");
+                            implemented = true;
+                            query = "select ks_code, ks_title, ks_level, ks_description\n" +
+                                    "from skill_set natural join knowledge_skill\n" +
+                                    "where cate_code = 1";
+                            break;
+
                         case 8:
                             implemented = true;
                             query = "(select ks_code, ks_title\n" +
@@ -79,6 +92,129 @@ class TUI {
                                     "(select ks_code, ks_title \n" +
                                     "from has_skill natural join knowledge_skill\n" +
                                     "where per_id = 4)";
+                            break;
+                        case 9:
+                            implemented = true;
+                            query = "with\n" +
+                                    "missing_ks(ks) as\n" +
+                                    "((select ks_code \n" +
+                                    "from required_skill natural join job\n" +
+                                    "where job_code = 1)\n" +
+                                    "minus\n" +
+                                    "(select ks_code\n" +
+                                    "from has_skill\n" +
+                                    "where per_id = 1))\n\n" +
+
+                                    "select c_code, c_title\n" +
+                                    "from course c\n" +
+                                    "where not exists\n" +
+                                    "((select *\n" +
+                                    "from missing_ks)\n" +
+                                    "minus\n" +
+                                    "(select ks_code\n" +
+                                    "from teaches_skill ts\n" +
+                                    "where ts.c_code = c.c_code))";
+                            break;
+                        case 13:
+                            implemented = true;
+                            query = "select cate_code, cate_title\n" +
+                                    "from job_category jc\n" +
+                                    "where not exists\n" +
+                                    "((select ks_code\n" +
+                                    "from skill_set ss\n" +
+                                    "where jc.cate_code = ss.cate_code)\n" +
+                                    "minus\n" +
+                                    "(select ks_code\n" +
+                                    "from has_skill\n" +
+                                    "where per_id = 2))";
+                            break;
+                        case 14:
+                            implemented = true;
+                            query = "with pay_rate_table as\n" +
+                                    "(select distinct rs.job_code\n" +
+                                    "from required_skill rs\n" +
+                                    "where not exists\n" +
+                                    "((select ks_code\n" +
+                                    "from required_skill rs2\n" +
+                                    "where rs.job_code = rs2.job_code)\n" +
+                                    "minus\n" +
+                                    "(select ks_code \n" +
+                                    "from has_skill\n" +
+                                    "where per_id = 1)))\n\n" +
+
+                                    "select job_code, cate_title,  pay_rate, pay_type\n" +
+                                    "from job natural join pay_rate_table natural join has_category\n" +
+                                    "natural join job_category\n" +
+                                    "where pay_rate = (select max(case when pay_type = 'salary' then pay_rate else pay_rate*1920 end)\n" +
+                                    "from job natural join pay_rate_table)";
+                            break;
+                        case 15:
+                            implemented = true;
+                            query = "select per_name, email\n" +
+                                    "from person p\n" +
+                                    "where not exists\n" +
+                                    "((select ks_code\n" +
+                                    "from required_skill\n" +
+                                    "where job_code = 1)\n" +
+                                    "minus\n" +
+                                    "(select ks_code\n" +
+                                    "from has_skill hs\n" +
+                                    "where hs.per_id = p.per_id))";
+                            break;
+                        case 16:
+                            implemented = true;
+                            query = "select per_id, per_name\n" +
+                                    "from person p\n" +
+                                    "where  1 = (select count(*)\n" +
+                                    "            from ((select ks_code\n" +
+                                    "                    from required_skill\n" +
+                                    "                    where job_code = 1)\n" +
+                                    "            minus\n" +
+                                    "            (select ks_code\n" +
+                                    "            from has_skill hs\n" +
+                                    "            where hs.per_id = p.per_id)))";
+                            break;
+                        case 21:
+                            implemented = true;
+                            query = "with unemployed(per_id) as\n" +
+                                    "((select per_id from person)\n" +
+                                    "minus\n" +
+                                    "(select per_id from works where end_date < CURRENT_DATE))\n\n" +
+                                    "select per_id\n" +
+                                    "from unemployed natural join job natural join has_category\n" +
+                                    "where cate_code = 1";
+                            break;
+                        case 22:
+                            implemented = true;
+                            query = "with unemployed(per_id) as\n" +
+                                    "((select per_id from person)\n" +
+                                    "minus\n" +
+                                    "(select per_id from works where end_date < CURRENT_DATE))\n" +
+                                    "select per_id\n" +
+                                    "from unemployed natural join works\n" +
+                                    "where  job_code = 8";
+                            break;
+                        case 23:
+                            implemented = true;
+                            query = "with\n" +
+                                    "company_size(comp_id, employee_count) as\n" +
+                                    "(select comp_id, count(*)\n" +
+                                    "from job natural join works\n" +
+                                    "group by comp_id)\n\n" +
+                                    "select comp_id employee_count\n" +
+                                    "from company_size\n" +
+                                    "where employee_count = ( select max ( employee_count ) from company_size)";
+                            break;
+                        case 24:
+                            implemented = true;
+                            query = "with\n" +
+                                    "sector_size(primary_sector, employee_count) as\n" +
+                                    "(select primary_sector, count(*)\n" +
+                                    "from job natural join works natural join company\n" +
+                                    "group by primary_sector)\n\n" +
+                                    "select primary_sector, employee_count\n" +
+                                    "from sector_size\n" +
+                                    "where employee_count = ( select max ( employee_count ) from sector_size)";
                             break;
                         default:
                             System.out.println();
