671c4ce0e5a8a52657b4823d80379b0b075f4669
Alexander Wilkerson <alex@uncorrected.com>
2017-11-20 18:30:46 -0600

    Added new stuff. Going to delete it now.

--- src/jobby/Controller.java
+++ src/jobby/Controller.java
@@ -1,9 +1,45 @@
 package jobby;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Controller {
+
+    @FXML
+    private TextField urlField;
+
+    private final Executor executor = Executors.newSingleThreadExecutor();
+
+    @FXML
     public void countWords(ActionEvent actionEvent) {
-        System.out.println("Foo");
+        try {
+            URL url = new URL(urlField.getText());
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                        String line;
+                        while((line = reader.readLine()) != null) {
+                            System.out.println(line);
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        } catch (MalformedURLException murle) {
+            throw new RuntimeException(murle);
+        }
+        System.out.println(urlField.getText());
     }
 }
