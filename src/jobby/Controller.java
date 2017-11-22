f9eaf0e92c7a7dd91fce7a777695dd57bb0de931
Alexander Wilkerson <alex@uncorrected.com>
2017-11-22 12:45:56 -0600

    updated person and company id field to auto increment on addition to database.

--- src/jobby/Controller.java
+++ src/jobby/Controller.java
@@ -1,26 +1,39 @@
 package jobby;
 
+import java.io.IOException;
 import java.sql.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 import java.sql.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller {
 
+    public Label accessLabel;
+    public String sessionName;
+    public String sessionUser;
+    public String sessionPassword;
+
     @FXML
     public TextArea responseField;
+    @FXML
     public TextArea queryField;
     @FXML
     private TextField passwordField;
     @FXML
     private TextField userField;
-    private String response = "";
-
     @FXML
-    private TextField urlField;
+    private String response = "";
 
     @FXML
     public void queryButton(ActionEvent actionEvent) {
@@ -28,7 +41,7 @@ public class Controller {
     }
 
     public boolean runQuery(String q) {
-        System.out.println(userField.getText());
+        System.out.println(sessionUser + " " + sessionPassword);
         if (q.trim().charAt(q.length() - 1) == ';') {
             q = q.trim().substring(0, q.length() - 1);
         }
@@ -42,7 +55,7 @@ public class Controller {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             // step 2 create the connection object
             Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@dbsvcs.cs.uno.edu:1521:orcl", userField.getText(), passwordField.getText());
+                    "jdbc:oracle:thin:@dbsvcs.cs.uno.edu:1521:orcl", sessionUser, sessionPassword);
             // step 3 create the statement object
             Statement stmt = con.createStatement();
             // step 4 execute query
@@ -79,4 +92,44 @@ public class Controller {
         return true;
     }
 
+    public void toPasswordInput(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.ENTER) {
+            passwordField.requestFocus();
+        }
+    }
+
+    public void authenticate(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.ENTER) {
+            if ((userField.getText().equals("awilkers")) && (passwordField.getText().equals("zT7RXLfP"))) {
+                accessGranted("Alex", "awilkers", "zT7RXLfP");
+
+                System.out.println("Welcome, Alex.");
+            } else if ((userField.getText().equals("kbongcas")) && (passwordField.getText().equals("9TTtPT97"))) {
+                accessGranted("Kevin", "kbongcas", "9TTtPT97");
+                System.out.println("Welcome, Kevin.");
+            } else{
+                accessLabel.setText("Access Denied!");
+                passwordField.setText("");
+                passwordField.requestFocus();
+            }
+        }
+    }
+
+    public void accessGranted(String name, String uname, String pword) {
+        sessionName = name;
+        sessionUser = uname;
+        sessionPassword = pword;
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("sample.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("Welcome, " + sessionName + "!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/jobby/Main.java
+++ src/jobby/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Jobby");
-        primaryStage.setScene(new Scene(root, 700, 800));
+        Parent root = FXMLLoader.load(getClass().getResource("login-screen.fxml"));
+        primaryStage.setTitle("Pending Matters Job Corp");
+        primaryStage.setScene(new Scene(root, 300, 200));
         primaryStage.show();
     }
 
